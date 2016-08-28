aeb7ab1a3025e9312787b5a7bc89610e828a1bdf
JUYOUNG-PC\kjykj <kjykjy0128@gmail.com>
2016-08-28 16:18:35 +0900

    main subs 추가

--- Calculator1/src/CalTest.java
+++ Calculator1/src/CalTest.java
@@ -11,10 +11,14 @@ public class CalTest {
 		switch (sc.nextInt()){
 		
 		case 1:
-			System.out.println("X를 입력하세요.");
-			Add.add(sc.nextInt(), sc.nextInt());
+			System.out.println("수 입력하세요.");
+			System.out.println(Add.add(sc.nextInt(), sc.nextInt()));
+			
+		
+		case 2:
+			System.out.println("수입력");
+			System.out.println(Substract.subst(sc.nextInt(), sc.nextInt()));
 		}
-	
 	}
 	
 	static void showMenu() {
