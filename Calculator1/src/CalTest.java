6e5525029ad44531aa38c49499aedd70f2e49fea
M90128\student <cuteleesk@gmail.com>
2016-08-28 15:45:59 +0900

    main1

--- Calculator1/src/CalTest.java
+++ Calculator1/src/CalTest.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class CalTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		showMenu();
+	}
+	
+	static int showMenu() {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.println("-- 메 뉴 --");
+		System.out.println("1. 더하기");
+		System.out.println("2. 빼기");
+		System.out.println("번호를 입력하세요._>");
+		num = sc.nextInt();
+		return num;
 	}
 
 }
